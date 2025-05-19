adbfc5d6c7390facc217c34e63fcb4ab3b6da743
d0vahzul <sonicaxy@gmail.com>
2025-05-19 23:10:15 +0300

    remove admin migration

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -3,7 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path("create-admin/", views.create_admin_user),
-    path("migrate/", views.run_migrate),
 ]
 
--- apps/main/views.py
+++ apps/main/views.py
@@ -1,27 +1,6 @@
 from django.shortcuts import render
 from .models import Header
 
-
-
 def index(request):
     header = Header.objects.all()
     return render(request, 'pages/index.html', {'header': header})
-
-
-from django.contrib.auth.models import User
-
-
-def create_admin_user(request):
-    if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser("admin", "admin@example.com", "sifre123")
-        return HttpResponse("Admin user created.")
-    return HttpResponse("User already exists.")
-
-
-
-from django.http import HttpResponse
-from django.core.management import call_command
-
-def run_migrate(request):
-    call_command('migrate')
-    return HttpResponse("Migrasyon tamamlandı.")
